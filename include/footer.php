229e297f6a444aee1fad539aad18e231b4ffc2d1
johnston-gerald <joh11028@byui.edu>
2015-04-30 17:27:33 -0700

    lesson 1

---
+++ include/footer.php
@@ -0,0 +1,18 @@
+<?php
+//    require_once('comments/model/database.php');
+//    require('comments/model/comment.php');
+//    require('comments/model/model.php');
+//    include 'comments/authentication/index.php';
+?>
+
+</main>
+
+<footer>
+<!--    <a href="index.php?action=assessments/deliberate-practice.php" title="View the Deliberate 
+           Practice Plan">Deliberate Practice</a>, 
+           <a href="index.php?action=assessments/site-plan.php" title="View the Site Plan">Site Plan</a>, 
+           <a href="index.php?action=switch_presentation/index.php" title="View the Teaching Presentation">Teaching Presentation</a>-->
+</footer>
+</div> <!-- end of wrapper div -->
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>%TITLE%</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="css/menu_style.css" rel="stylesheet" type="text/css" media="screen">
+<!--    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>-->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="author" content="Gerald Johnston">
+</head>
+
+<body>
+    <header><h1>Gerald's CMS</h1></header>
+
+    <nav>
+        <ul id="top_menu">
+            <li><a href='index.php'>home</a></li>
+            <li><a href='index.php?action=assignments/assignments.php'>assignments</a>
+                <ul>
+<!--                    <li><a href='#'>Web development</a>
+                        <ul>
+                            <li><a href='index.php?action=tutorials/web_development/html_structures.php'>HTML structures</a></li>
+                            <li><a href='index.php?action=tutorials/web_development/ieshiv.php'>Internet Explorer shiv</a></li>
+                            <li><a href='index.php?action=tutorials/web_development/developer_tools.php'>Google Chrome Developer Tools</a></li>
+                            <li><a href='index.php?action=tutorials/web_development/css_2.php'>CSS Part Two (coming soon)</a></li>
+                            <li><a href='index.php?action=tutorials/web_development/php_1.php'>PHP Part One (coming soon)</a></li>
+                            <li><a href='index.php?action=tutorials/web_development/php_2.php'>PHP Part Two (coming soon)</a></li>
+                        </ul>
+                    </li>-->
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div id="wrapper">
+    <main>
